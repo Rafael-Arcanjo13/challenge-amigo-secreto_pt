fd0f938447c219ddbe7149cbda0d0d0c0a1b0b01
Rafael Arcanjo <rafael.arcanjo.dssilva@gmail.com>
2025-08-10 00:19:36 -0300

    Primeira part

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const input = document.querySelector('.input-name');
+const btn = document.querySelector('.button-add');
+const nameList = document.querySelector('.name-list');
+const ress = document.querySelector('result-list');
+
+btn.addEventListener('click', () => {
+    const li = document.createElement('li');
+    const buttonClear = document.createElement('button')
+    nameList.appendChild(li);
+    nameList.appendChild(buttonClear);
+
+
+    li.innerHTML += input.value;
+});
